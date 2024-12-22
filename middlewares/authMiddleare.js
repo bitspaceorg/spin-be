728bd917aa9342a03b9232b3fefe91a5bf53ae18
Surya Prakash <surya0prakash@proton.me>
2024-12-22 14:33:47 +0530

    [Fix] status code fix middleware 👀

--- middlewares/authMiddleare.js
+++ middlewares/authMiddleare.js
@@ -3,15 +3,16 @@ import { verifyToken } from "../utils/jwt.js";
 export const useAuth = (req, res, next) => {
 	const token = req.cookies.token;
 	if (!token || token == "") {
-		res.json({
+		res.status(400).json({
 			message: "Invalid Login!",
 		});
 		return;
 	}
 	try {
 		req.user = verifyToken(token);
-	} catch (err) { res.cookie("token", {});
-		res.json({
+	} catch (err) { 
+		res.cookie("token", {});
+		res.status(400).json({
 			message: "Invalid Login!",
 		});
 		return;
