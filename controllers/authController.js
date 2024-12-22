7fae615a316320c422e05f06f87fa3a3f4468b2c
Surya Prakash <surya0prakash@proton.me>
2024-12-22 13:41:14 +0530

    [Feat] add auth verify 🔐

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,74 +3,83 @@ import { checkHash, hashPassword } from "../utils/hash.js";
 import { signToken } from "../utils/jwt.js";
 import { createUserSchema, loginSchema } from "../validators/authValidator.js";
 
+export async function handleVerify(req, res) {
+  return res.status(200).json({
+    message: "Valid user!",
+    data: {
+      id: req.user.id,
+      role: req.user.role,
+    },
+  });
+}
 export async function handleLogin(req, res) {
-	const user = await req.body;
-	try {
-		const data = await loginSchema.parseAsync(user);
-		const dbUser = await db.getUser(data.email);
-		if (!dbUser[0]) {
-			res.status(400).json({
-				message: "Invalid User Credentials!",
-			});
-			return;
-		}
-		if (!checkHash(data.password, dbUser[0].password)) {
-			res.status(400).send("Failed to login user!");
-			return;
-		}
-		const token = signToken({
-			id: dbUser[0].id,
-			role: dbUser[0].role,
-		});
+  const user = await req.body;
+  try {
+    const data = await loginSchema.parseAsync(user);
+    const dbUser = await db.getUser(data.email);
+    if (!dbUser[0]) {
+      res.status(400).json({
+        message: "Invalid User Credentials!",
+      });
+      return;
+    }
+    if (!checkHash(data.password, dbUser[0].password)) {
+      res.status(400).send("Failed to login user!");
+      return;
+    }
+    const token = signToken({
+      id: dbUser[0].id,
+      role: dbUser[0].role,
+    });
 
-		res.cookie("token", token, {
-			maxAge: 1000 * 3600, //1 hours
-			secure: false,
-			httpOnly: false,
-			sameSite: "lax",
-		});
-		res.json({
-			message: "Login Successful!",
-			data: {
-				id: dbUser[0].id,
-				role: dbUser[0].role,
-			},
-		});
-	} catch (err) {
-		console.error("ERROR: ", err.message);
-		res.status(400).json({
-			message: "Login Failed!",
-			data: err.message,
-		});
-	}
+    res.cookie("token", token, {
+      maxAge: 1000 * 3600, //1 hours
+      secure: false,
+      httpOnly: false,
+      sameSite: "lax",
+    });
+    res.json({
+      message: "Login Successful!",
+      data: {
+        id: dbUser[0].id,
+        role: dbUser[0].role,
+      },
+    });
+  } catch (err) {
+    console.error("ERROR: ", err.message);
+    res.status(400).json({
+      message: "Login Failed!",
+      data: err.message,
+    });
+  }
 }
 
 export async function handleSignup(req, res) {
-	const user = await req.body;
-	try {
-		const data = await createUserSchema.parseAsync(user);
-		data.password = hashPassword(data.password);
-		const id = await db.createUser(
-			data.name,
-			data.email,
-			data.role,
-			data.password,
-		);
-		res.status(201).json({
-			message: "user created successfully!",
-			user_id: id,
-		});
-	} catch (err) {
-		console.error("ERROR: ", err.message, err.code);
-		if (err.code == "23505") {
-			res.status(400).json({
-				message: "User Already Exists!",
-			});
-		} else {
-			res.status(400).json({
-				message: "Failed to Create User!",
-				data: err.message,
-			});
-		}
-	}
+  const user = await req.body;
+  try {
+    const data = await createUserSchema.parseAsync(user);
+    data.password = hashPassword(data.password);
+    const id = await db.createUser(
+      data.name,
+      data.email,
+      data.role,
+      data.password,
+    );
+    res.status(201).json({
+      message: "user created successfully!",
+      user_id: id,
+    });
+  } catch (err) {
+    console.error("ERROR: ", err.message, err.code);
+    if (err.code == "23505") {
+      res.status(400).json({
+        message: "User Already Exists!",
+      });
+    } else {
+      res.status(400).json({
+        message: "Failed to Create User!",
+        data: err.message,
+      });
+    }
+  }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 export const router = express.Router();
-import { handleLogin, handleSignup } from "../controllers/authController.js";
+import { handleLogin, handleSignup, handleVerify } from "../controllers/authController.js";
+import { useAuth } from "../middlewares/authMiddleare.js";
 
 router.post("/login", handleLogin);
 router.post("/signup", handleSignup);
+router.post("/verify",useAuth,handleVerify);
